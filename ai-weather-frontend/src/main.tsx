846e1456a70c869b0813572b97fbf8e2f321a20a
Akshey VS <aksheyvs@gmail.com>
2026-02-20 10:01:34 +0530

    setup QueryClient

--- ai-weather-frontend/src/main.tsx
+++ ai-weather-frontend/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 10000 * 60 * 5,
+            refetchOnWindowFocus: false,
+            retry: 1,
+        },
+    },
+});
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     </StrictMode>,
 );
