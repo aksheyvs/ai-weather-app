4f179c5bc8ff94752b891d53abff82ee4f483c5b
Akshey VS <aksheyvs@gmail.com>
2026-02-20 12:57:55 +0530

    Update axios response interceptor

--- ai-weather-frontend/src/api/axios.ts
+++ ai-weather-frontend/src/api/axios.ts
@@ -12,4 +12,17 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
+api.interceptors.response.use(
+    (Response) => Response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("token");
+
+            window.location.href = "/login";
+        }
+
+        return Promise.reject(error)
+    }
+);
+
 export default api;
