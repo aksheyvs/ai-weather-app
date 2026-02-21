be3202f61bc66ab1a82ad8bac5dd06c11b23a441
Akshey VS <aksheyvs@gmail.com>
2026-02-21 15:30:43 +0530

    update server.ts and dailySummary

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 import app from "./index.js"
-import "./jobs/emailAlerts.cron.js"
+
 
 const port: number = + (process.env.PORT ?? 3000)
 
 app.listen(port, () => {
-    console.log(`app listeing on port ${port}`)
+    console.log(`app listening on port ${port}`)
 });
