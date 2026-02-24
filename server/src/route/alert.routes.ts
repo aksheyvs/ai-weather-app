3d188e3961c2940698c14bd24269b3adc040660f
Akshey VS <aksheyvs@gmail.com>
2026-02-24 11:16:06 +0530

    alert routes

--- server/src/route/alert.routes.ts
+++ server/src/route/alert.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
-import { createConditionAlert } from "../controller/alert.controller.js";
+import { getAlerts, createConditionAlert } from "../controller/alert.controller.js";
 
 const router = Router();
+
+router.get("/", authenticate, getAlerts);
 
 router.post("/condition", authenticate, createConditionAlert);
 
