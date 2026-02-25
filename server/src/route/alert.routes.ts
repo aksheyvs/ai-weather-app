f4ba89f6853776b61e52ddc0cc76379d1e975a4e
Akshey VS <aksheyvs@gmail.com>
2026-02-25 08:58:37 +0530

    add routes

--- server/src/route/alert.routes.ts
+++ server/src/route/alert.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
-import { getAlerts, createConditionAlert } from "../controller/alert.controller.js";
+import { getAlerts, createConditionAlert, deleteAlert, updateAlert } from "../controller/alert.controller.js";
 
 const router = Router();
 
 router.get("/", authenticate, getAlerts);
 
 router.post("/condition", authenticate, createConditionAlert);
+
+router.delete("/:id", authenticate, deleteAlert);
+
+router.patch("/:id", authenticate, updateAlert);
 
 export default router;
